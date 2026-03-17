afc5c0b6591d399bbe031ee1da39a5181fbcb5da
jaredvisca <jared.visca@gmail.com>
2026-03-17 19:33:06 -0400

    Testing to make sure the right user commits code now

--- src/main/java/com/learning/jspringalgos/JSpringAlgosApplication.java
+++ src/main/java/com/learning/jspringalgos/JSpringAlgosApplication.java
@@ -9,5 +9,36 @@ import java.util.stream.Collectors;
 @SpringBootApplication
 public class JSpringAlgosApplication {
 
-    public static void main(String[] args) {SpringApplication.run(JSpringAlgosApplication.class, args);}
+    public static void main(String[] args) {
+        SpringApplication.run(JSpringAlgosApplication.class, args);
+        int[] foo = {0, 2, 1, 3, 2};
+        int result = findDuplicate(foo);
+        System.out.println(result);
+
+//        countToNumber(3);
+//        countToNumber(4);
+
+    }
+
+    public static int findDuplicate(int[] array) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        System.out.println(Arrays.toString(array));
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+        for(int i : array) {
+            if(!map.containsKey(i)) {
+                map.put(i, 1);
+            } else {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    public static void countToNumber(int n) {
+        int numberToPrintTo = Math.powExact(10, n);
+        for(int i = 0; i < numberToPrintTo; i++) {
+            System.out.println(i);
+        }
+    }
 }
